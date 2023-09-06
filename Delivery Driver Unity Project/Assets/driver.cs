39d1aaab3ed96d21374aafc1988cc043314b54b1
JeremyRoalef <roalef944@morrisville.edu>
2023-09-05 21:37:36 -0400

    Using Input.GetAxis()

--- Delivery Driver Unity Project/Assets/driver.cs
+++ Delivery Driver Unity Project/Assets/driver.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class driver : MonoBehaviour
 {
     //note to self: don't assign variables in Start()
-    [SerializeField]  float fltSteerSpeed = 1f;
+    [SerializeField]  float fltSteerSpeed = .5f;
     [SerializeField]  float fltMoveSpeed = 0.01f;
 
     // Start is called before the first frame update
@@ -17,8 +17,10 @@ public class driver : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, 0, fltSteerSpeed);
-        transform.Translate(0, fltMoveSpeed, 0);
+        float fltSteerAmount = UnityEngine.Input.GetAxis("Horizontal") * fltSteerSpeed;
+        float fltMoveAmount = UnityEngine.Input.GetAxis("Vertical") * fltMoveSpeed;
+        transform.Rotate(0, 0, -fltSteerAmount);
+        transform.Translate(0, fltMoveAmount, 0);
     }
 }
 
